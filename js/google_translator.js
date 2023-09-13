d3e69f7127b714a42e7760673014492dbd19fd7a
Mus9901 <mustafoirgashev9901@gmail.com>
2023-09-13 11:23:44 +0100

    first commit

---
+++ js/google_translator.js
@@ -0,0 +1,9 @@
+/* Current script is functioning in the html file:*/
+									/* In All HTML */
+
+			function googleTranslateElementInit() { 
+			new google.translate.TranslateElement(
+				{pageLanguage: 'en'}, 
+				'google_translate_element'
+			); 
+			} 
